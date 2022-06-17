0992a93df69cb38057561882f982ab05b79c4ce0
zhulin3141 <zhulin3141@gmail.com>
2022-06-18 02:13:14 +0800

    fix china.js

--- src/assets/china.js
+++ src/assets/china.js
@@ -1,16 +1,11 @@
-import * as echarts from 'echarts';
-(function(root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['exports', 'echarts'], factory);
-    } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
-        // CommonJS
-        factory(exports, require('echarts'));
-    } else {
-        // Browser globals
-        factory({}, echarts);
-    }
-}(this, function(exports, echarts) {
+import * as echarts from "echarts/core";
+
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, echarts) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.echarts = {}, echarts));
+}(this, function (exports, echarts) {
+    
     var log = function(msg) {
         if (typeof console !== 'undefined') {
             console && console.error && console.error(msg);
