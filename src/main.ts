bb48f357d43ff320aa24b22f951c6e5b3e992725
zhulin3141 <zhulin3141@gmail.com>
2022-06-18 15:04:07 +0800

    fix echarts import

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import * as echarts from 'echarts'
 import "./utils/mockServe"
 
 const app = createApp(App)
-app.config.globalProperties.$echarts = echarts
 app.mount('#app')
