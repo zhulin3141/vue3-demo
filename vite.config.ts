50d34d6059df1c85f06d6774b2f0240f5ee24914
zhulin3141 <zhulin3141@gmail.com>
2022-06-18 14:09:47 +0800

    env

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,45 @@
+/**
+ * @type {import('vite').UserConfig}
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      less: {
-        charset: false,
+export default ({ mode }) => {
+  const __DEV__ = mode === 'development'
+
+  return defineConfig({
+    base: __DEV__ ? '/' : '/vue3-demo/',
+    plugins: [vue(), visualizer()],
+    // 设置路径别名
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
       },
     },
-  },
-})
+    css: {
+      preprocessorOptions: {
+        less: {
+          charset: false,
+        },
+      },
+    },
+    build: {
+      sourcemap: false,
+      outDir: 'dist', //指定输出路径
+      assetsDir: 'assets', // 指定生成静态资源的存放路径
+      minify: __DEV__ ? false : true,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vue: ['vue'],
+            echarts: ['echarts'],
+            mockjs: ['mockjs'],
+          }
+        }
+      },
+    },
+  })
+}
